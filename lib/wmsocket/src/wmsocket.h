0a3620f1e8ec2e44438eb230766a59ae805051ae
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-21 16:27:38 +0100

    patch

--- lib/wmsocket/src/wmsocket.h
+++ lib/wmsocket/src/wmsocket.h
@@ -4,19 +4,19 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-enum DataType {
+enum DataType
+{
     BUTTON,
     SENSOR,
     RGBLED
 };
- 
 
-void setupWiFiconnection(const char* ssid, const char* password);
+void setupWiFiconnection(const char *ssid, const char *password);
 
-WiFiClient connectSocket(const char* host, int port);
+WiFiClient connectSocket(const char *host, int port);
 
 void sendToServer(WiFiClient &client, DataType type, int value);
 
-int listenForData(WiFiClient &client, DataType* type, int *value);
+int listenForData(WiFiClient &client, DataType *type, int *value);
 
 #endif // WMSOCKET_H
--- src/wemosrgb.cpp
+++ src/wemosrgb.cpp
@@ -1,5 +1,5 @@
-//RGB LED OP WEMOS D1 MINI
-// AANGESTUURD VIA SOCKET MET RPIB
+// RGB LED OP WEMOS D1 MINI
+//  AANGESTUURD VIA SOCKET MET RPIB
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "wmsocket.h"
@@ -8,14 +8,15 @@
 #define RGB_G D6
 #define RGB_B D7
 
-const char* ssid = "707";
-const char* password = "DEFCAB1960";
-const char* hostname = "bramsvoorhoofd.local";
+const char *ssid = "707";
+const char *password = "DEFCAB1960";
+const char *hostname = "bramsvoorhoofd.local";
 const int port = 8069;
 
 WiFiClient client;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     setupWiFiconnection(ssid, password);
     client = connectSocket(hostname, port);
@@ -25,14 +26,17 @@ void setup() {
     pinMode(RGB_B, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
     enum DataType type;
     int value;
     int readStatus = listenForData(&client, &type, &value);
     Serial.println("type: " + String(type) + ", value: " + String(value));
 
-    if (readStatus > 0) {
-        if (type == RGBLED) {
+    if (readStatus > 0)
+    {
+        if (type == RGBLED)
+        {
             analogWrite(RGB_R, (value >> 16) & 0xFF);
             analogWrite(RGB_G, (value >> 8) & 0xFF);
             analogWrite(RGB_B, value & 0xFF);
