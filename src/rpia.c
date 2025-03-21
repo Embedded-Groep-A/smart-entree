9417736dc5d25b9fd57fc33ad31dc5fe21803176
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-21 14:01:22 +0100

    test

--- src/rpia.c
+++ src/rpia.c
@@ -11,12 +11,17 @@
 
 int main() {
     int client_fd = connectSocket(HOST, PORT);
-    sendToServer(client_fd, BUTTON, 1);
-    sleep(1);
-    sendToServer(client_fd, BUTTON, 0);
-    sleep(5);
+    // sendToServer(client_fd, BUTTON, 1);
+    // sleep(1);
+    // sendToServer(client_fd, BUTTON, 0);
+    // sleep(5);
 
-    disconnectSocket(client_fd);
+    // disconnectSocket(client_fd);
+    while (1) {
+        sendToServer(client_fd, RGBLED, 0);
+        sleep(1);
+
+    }
     return 0;
 }
 
