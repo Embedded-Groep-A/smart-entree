cb8967e9609ee01165dcada2042d2222f5ce4107
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-21 14:32:13 +0100

    test

--- src/rpib.c
+++ src/rpib.c
@@ -7,8 +7,6 @@
 #define PORT 8069
 #define MAX_CLIENTS 4
 
-static int client_handled = 0;
-
 int main() {
     int server_fd = hostSocket(PORT);
     initClientArray();
@@ -30,23 +28,19 @@ int main() {
         }
 
         int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-        client_handled = 0;
-        if (!client_handled && FD_ISSET(server_fd, &read_fds)) {
+        printf("Activity: %d\n", activity);
+        if (FD_ISSET(server_fd, &read_fds)) {
             int client_fd = acceptClient(server_fd);
 
             if (client_fd >= 0) {
-                client_handled = 1;
-                if (client_fd >= 0) {
-                    int client_id = assignClientId();
-                    if (client_id == -1) {
-                        printf("Max clients reached. Rejecting new client.\n");
-                        close(client_fd);
-                    } else {
-                        clients[client_id].socket = client_fd;
-                        clients[client_id].id = client_id;
-                        printf("New client connected, assigned ID: %d\n", client_id);
-
-                    }
+                int client_id = assignClientId();
+                if (client_id == -1) {
+                    printf("Max clients reached. Rejecting new client.\n");
+                    close(client_fd);
+                } else {
+                    clients[client_id].socket = client_fd;
+                    clients[client_id].id = client_id;
+                    printf("New client connected, assigned ID: %d\n", client_id);
                 }
             }
         }
@@ -67,5 +61,4 @@ int main() {
                 }
             }
         }
-    }
 }
