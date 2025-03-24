91265d195ea516831d6509e5cff440b836430e47
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-24 19:56:01 +0100

    i2c beter

--- src/rpia.c
+++ src/rpia.c
@@ -4,25 +4,37 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
-#include <wiringPiI2C.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
 #include "pisocket.h"
 
 #define PORT 8069
 #define HOST "bramsvoorhoofd.local"
-#define I2C_ADDR 0x11 // Replace with your I2C device address
+
+#define I2C_DEVICE "/dev/i2c-1"
+#define I2C_ADDR   0x20  
 
 uint8_t rgbValues[3] = {255, 255, 255};
 
 int main() {
-    int client_fd = connectSocket(HOST, PORT);
+    int file = open(I2C_DEVICE, O_RDWR);
+    ioctl(file, I2C_SLAVE, I2C_ADDR);
 
-    int i2c_fd = wiringPiI2CSetup(I2C_ADDR);
+    while (1) {
+        char buffer[6];
+        int bytesRead = read(file, buffer, 6);
+        if (bytesRead < 0) {
+            perror("Failed to read from the i2c bus");
+        } else {
+            printf("Data read: ");
+            for (int i = 0; i < bytesRead; i++) {
+            printf("%02x ", buffer[i]);
+            }
+            printf("\n");
+        }
 
-
-    // Read data from I2C device
-    int data = wiringPiI2CRead(i2c_fd);
-
-    printf("Received data: %d\n", data);
+    }
 
     return 0;
 }
