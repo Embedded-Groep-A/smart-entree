1074233e029ed236986bd82ef1c866cf10f5bb1b
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-24 19:51:37 +0100

    i2c

--- src/rpia.c
+++ src/rpia.c
@@ -4,26 +4,25 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include <wiringPiI2C.h>
 #include "pisocket.h"
 
 #define PORT 8069
 #define HOST "bramsvoorhoofd.local"
+#define I2C_ADDR 0x11 // Replace with your I2C device address
 
+uint8_t rgbValues[3] = {255, 255, 255};
 
 int main() {
     int client_fd = connectSocket(HOST, PORT);
-    uint8_t rgbValues[3] = {0, 85, 170};
 
-    while (1) {
-        sendToServer(client_fd, RGBLED, rgbValues, 3);
-        rgbValues[0] = (rgbValues[0] + 1) % 256;
-        rgbValues[1] = (rgbValues[1] + 1) % 256;
-        rgbValues[2] = (rgbValues[2] + 1) % 256;
-        usleep(10000);
+    int i2c_fd = wiringPiI2CSetup(I2C_ADDR);
 
-    }
+
+    // Read data from I2C device
+    int data = wiringPiI2CRead(i2c_fd);
+
+    printf("Received data: %d\n", data);
+
     return 0;
 }
-
-
-
