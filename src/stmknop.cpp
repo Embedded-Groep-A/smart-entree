27854e51da879fc9e6c92e07c7e18fa36ada4f86
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-03-26 17:40:09 +0100

    i2c

--- src/stmknop.cpp
+++ src/stmknop.cpp
@@ -1,6 +1,25 @@
+#include <Arduino.h>
 #include <Wire.h>
 
 #define I2C_ADDR 0x08
+
+volatile uint8_t receivedCommand = 0;
+
+
+void receiveEvent(int bytes) {
+  if (Wire.available()) {
+      receivedCommand = Wire.read();
+      Serial.print("Received command: ");
+      Serial.println(receivedCommand);
+  }
+}
+
+void requestEvent() {
+  uint8_t dataToSend = receivedCommand + 10;  // Example response
+  Wire.write(dataToSend);
+  Serial.print("Sent response: ");
+  Serial.println(dataToSend);
+}
 
 void setup() {
     Wire.begin(I2C_ADDR);
@@ -10,22 +29,8 @@ void setup() {
     Serial.println("STM32 I2C Slave Ready.");
 }
 
-volatile uint8_t receivedCommand = 0;
 
-void receiveEvent(int bytes) {
-    if (Wire.available()) {
-        receivedCommand = Wire.read();
-        Serial.print("Received command: ");
-        Serial.println(receivedCommand);
-    }
-}
 
-void requestEvent() {
-    uint8_t dataToSend = receivedCommand + 10;  // Example response
-    Wire.write(dataToSend);
-    Serial.print("Sent response: ");
-    Serial.println(dataToSend);
-}
 
 void loop() {
     // Do other processing if needed
