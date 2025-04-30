656f67ad32e757db8ddd0e0e903130306d0b909d
CasBeekhuizen <casbeekhuizen018@gmail.com>
2025-04-30 10:03:11 +0200

    add define for communications standaards

---
+++ src/define.h
@@ -0,0 +1,14 @@
+//Communication standard RS485 Groep A
+
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define ACCEPT  0x66
+#define WEIGEREN 0x05
+#define OPEN 0x02
+#define BEL 0x02
+#define UID 0x01
+#define MSG 1D
+#define BRAND 0x04
+#endif // DEFINE_H
+
