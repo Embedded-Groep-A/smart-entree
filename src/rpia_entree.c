9d48e2b61f526a530d4510349c5636d94f3eebfe
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-01 15:00:25 +0200

    test

--- src/rpia_entree.c
+++ src/rpia_entree.c
@@ -64,7 +64,7 @@ int main() {
                 printf("Received: %s\n", buffer);
                 if (strncmp(buffer, "UID:", 4) == 0) {
                     uint8_t uid[4];
-                    sscanf(buffer + 4, "%hhx%hhx%hhx%hhx", &uid[0], &uid[1], &uid[2], &uid[3]);
+                    sscanf(buffer + 4, "%hhx %hhx %hhx %hhx", &uid[0], &uid[1], &uid[2], &uid[3]);
                     printf("UID: %02X %02X %02X %02X\n", uid[0], uid[1], uid[2], uid[3]);
                     int eigenaarIndex = checkUID(uid);
                     if (eigenaarIndex != -1) {
