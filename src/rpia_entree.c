c53f0116f09b6e9fbf15e965a4c50dd467125c96
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-05-07 11:11:56 +0200

    puts

--- src/rpia_entree.c
+++ src/rpia_entree.c
@@ -59,7 +59,7 @@ int main() {
     int index = 0;
 
     while (1) {
-        serialWrite(fd, "OPEN\n", 5);
+        serialPuts(fd, "OPEN\n", 5);
         serialFlush(fd);
         usleep(1000000); // 1 second delay
 
